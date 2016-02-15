e2e69c9c59041c154920fc9e3d9cc5ae5b0093fc
andersonmcook <andersonmcook@gmail.com>
2016-02-15 15:12:59 -0600

    initial quote commit

---
+++ server.js
@@ -0,0 +1,58 @@
+/* eslint-disable */
+
+'use strict';
+
+const express = require('express');
+const app = express();
+const router = express.Router();
+// needs to have a dynamic port for heroku to work
+const PORT = process.env.PORT || 3000;
+const path = require('path');
+const nodeSassMiddleware = require('node-sass-middleware');
+const bodyParser = require('body-parser');
+const request = require('request');
+const mongoose = require('mongoose');
+
+const MONGODB_HOST = process.env.MONGODB_HOST || 'localhost';
+const MONGODB_PORT = process.env.MONGODB_PORT || 27017;
+const MONGODB_USER = process.env.MONGODB_USER || '';
+const MONGODB_PASS = process.env.MONGODB_PASS || '';
+const MONGODB_DB = 'node-stocks';
+const MONGODB_URL_PREFIX = MONGODB_USER ? `${MONGODB_USER}:${MONGODB_PASS}@` : '';
+
+const MONGODB_URL = `mongodb://${MONGODB_URL_PREFIX}${MONGODB_HOST}:${MONGODB_PORT}/${MONGODB_DB}`;
+
+// set view engine to a file ending with .jade in the views folder by default
+app.set('view engine', 'jade');
+
+// body-parser middleware
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
+app.use(bodyParser.json());
+
+//node sass middleware
+app.use(nodeSassMiddleware({
+  src: path.join(__dirname, 'public'),
+  dest: path.join(__dirname, 'public'),
+  indentedSyntax: true,
+  sourceMap: true,
+  outputStyle: 'compressed',
+  debug: true
+}));
+
+// able to use static files
+// app.use(express.static('public'));
+
+// app.use(routes);
+
+mongoose.connect(MONGODB_URL);
+
+mongoose.connection.on('open', () => {
+  console.log("MONGO OPEN");
+
+  // listen for requests
+  app.listen(PORT, () => {
+    console.log(`Node.js server started. Listening on port ${PORT}`);
+  });
+});
