d2a0303aa5d3d069fc163bf26ec06bb706696187
andersonmcook <andersonmcook@gmail.com>
2016-02-15 20:15:44 -0600

    jade templating a table

---
+++ public/controllers/quote.controller.js
@@ -0,0 +1,17 @@
+
+// angular.module('quoteCtrl', [])
+
+// // inject the Todo service factory into our controller
+// .controller('quoteCtrl', ['$scope','$http', ($scope, $http) => {
+//   $scope.formData = {};
+//   $scope.loading = true;
+
+//   // GET =====================================================================
+//   // when landing on the page, get all todos and show them
+//   // use the service to get all the todos
+//   // Todos.get()
+//   //   .success(function(data) {
+//   //     $scope.todos = data;
+//   //     $scope.loading = false;
+//     // });
+// }]);
--- quote/quote.route.js
+++ quote/quote.route.js
@@ -4,7 +4,12 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/quote', (req, res) => {
-  res.render('quote');
+  const test = [{symbol: "AAPL", price: 76.2}, {symbol: "BBPL", price: 9.1}, {symbol: "CCPL", price: 54.3}];
+  res.render('quote', {stuffs: test});
 });
+
+// router.post('/quote', (req, res) => {
+
+// });
 
 module.exports = router;
